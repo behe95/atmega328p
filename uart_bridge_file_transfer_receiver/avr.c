22680b1ab968b02a8ea287fe1d48297bdb2775cf
behe95 <65931945+behe95@users.noreply.github.com>
2026-07-03 19:33:28 -0400

    IMPLEMENT: circular buffer data struct

--- uart_bridge_file_transfer_receiver/avr.c
+++ uart_bridge_file_transfer_receiver/avr.c
@@ -5,14 +5,29 @@
 #define FOSC 16000000
 #define BAUD 9600
 #define UBRR (((FOSC/16)/BAUD) - 1)
+#define BUFFER_SIZE 128
+
+typedef struct _TCircBuff {
+  volatile char* ptrRead;
+  volatile char* ptrWrite;
+  volatile char buffer[BUFFER_SIZE];
+} _TCircBuff;
 
 volatile uint8_t c; 
 volatile int tx_ready;
 
+volatile _TCircBuff buffer;
+
 void UART_init();
+void initBuf(_TCircBuff* buf);
+
+char readBuf(_TCircBuff* buf);
+void writeBuf(_TCircBuff* buf, char c);
 
 
 int main(void) {
+  initBuf(&buffer);
+
   cli();
   UART_init();
 
@@ -48,8 +63,7 @@ void UART_init() {
 
 // USART Rx Complete
 ISR(USART_RX_vect) {
-  c = UDR0;
-  tx_ready = 1;
+  writeBuf(&buffer, UDR0);
 
   // enable data register empty interrupt
   UCSR0B |= (1 << UDRIE0);
@@ -57,17 +71,57 @@ ISR(USART_RX_vect) {
 
 // USART Data Register Empty
 ISR(USART_UDRE_vect) {
-  if(tx_ready) {
-    UDR0 = c;
-    tx_ready = 0;
-  } else {
-    
+  char c = readBuf(&buffer);
+  if(c == 0) {
+      
     // disable data register empty interrupt
     UCSR0B &= ~(1 << UDRIE0);
+    return;
   }
+  
+  UDR0 = c;
+
+
+  
 }
 
 // USART Tx Complete
 ISR(USART_TX_vect) {
+
+}
+
+
+void initBuf(_TCircBuff* buf) {
+  buf->ptrRead = buf->buffer;
+  buf->ptrWrite = buf->buffer;
+}
+
+
+
+char readBuf(_TCircBuff* buf) {
+  if(buf->ptrRead == buf->ptrWrite) {
+    return 0;
+  }
+
+  char c = *(buf->ptrRead);
+  buf->ptrRead += 1;
+
+  // ensure not exceeding size
+  // if exceeding start from the 0
+  if(buf->ptrRead >= (buf->buffer + BUFFER_SIZE)) {
+    buf->ptrRead = buf->buffer;
+  }
+
+  return c;
+}
+void writeBuf(_TCircBuff* buf, char c) {
+
+  char* next = buf->ptrWrite + 1;
+  
+  if(next >= (buf->buffer + BUFFER_SIZE)) {
+    next = buf->buffer;
+  }
+  *(buf->ptrWrite) = c;
+  buf->ptrWrite = next;
 
 }
