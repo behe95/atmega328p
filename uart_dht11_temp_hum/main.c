be1ea3ffcd3e52a2659527c03126d1ee4903aaae
behe95 <65931945+behe95@users.noreply.github.com>
2026-08-15 01:30:36 -0400

    ADD: system up time

--- uart_dht11_temp_hum/main.c
+++ uart_dht11_temp_hum/main.c
@@ -3,6 +3,7 @@
 * ref: https://components101.com/sites/default/files/component_datasheet/DFR0067%20DHT11%20Datasheet.pdf
 *
 *	ADD: CMD Line for User Input
+*	Add: Stopwatch
 */
 
 
@@ -23,6 +24,36 @@ void handle_cmd(char* line);
 uint8_t str_eq(const char* a, const char* b);
 
 
+// state
+typedef enum {
+	STATE_COUNTING,
+	STATE_ALARMING,
+	STATE_IDLE
+} alarm_state_t;
+
+volatile uint32_t ms_counter = 0;
+volatile uint8_t new_second = 0;
+volatile uint32_t second_counter = 0;
+volatile uint16_t day_counter = 0;
+//volatile alarm_state_t state = STATE_COUNTING;
+//volatile uint16_t countdown_remaining = 10;
+//volatile uint16_t blink_period_ms = 1000;
+//volatile uint16_t blink_timer_ms = 0;
+
+
+ISR(TIMER0_COMPA_vect) {
+	ms_counter++;
+	if(ms_counter >= 1000) {
+		ms_counter = 0;
+		new_second = 1;
+		second_counter++;
+		if(second_counter >= 86400) {
+			second_counter = 0;
+			day_counter++;
+		}
+	}
+
+}
 
 
 
@@ -32,21 +63,29 @@ int main(void) {
 	//PORTD &= ~(1 << PORTD2);
 	//while(1);
 
-
+	// uart config
 	// baud rate
 	UBRR0H = 0;
 	UBRR0L = 103;
 
 	UCSR0B = (1 << TXEN0) | (1 << RXEN0) | (1 << RXCIE0);
-	
-
 	UCSR0C = (1 << UCSZ01) | (1 << UCSZ00);
 
-	sei();
-	_delay_ms(1000);
 
-	int temperature = -5;
-	unsigned long counter = 0;
+	// gpio config
+	DDRB = (1 << DDB5);
+
+	// timer0 config
+	TCCR0A = (1 << WGM01);
+	TCCR0B = (1 << CS01) | (1 << CS00);
+	OCR0A = 249;
+	TIMSK0 = (1 << OCIE0A);
+
+	sei();
+	//_delay_ms(1000);
+
+	//int temperature = -5;
+	//unsigned long counter = 0;
 	
 
 	char line[LINE_MAX];
@@ -54,7 +93,8 @@ int main(void) {
 	uart_puts("Type 'help' to show available commands\r\n");
 
 	while(1) {
-		
+		//PORTB |= (1 << PORTB5);
+
 		uart_puts("> ");
 		uart_readline(line, LINE_MAX);
 		handle_cmd(line);
@@ -116,6 +156,7 @@ void handle_cmd(char* line) {
 		uart_puts("        Commands        \r\n");
 		uart_puts("    1. show_temp\r\n");
 		uart_puts("    2. show_humidity\r\n");
+		uart_puts("    3. sys_uptime\r\n");
 	} else if(str_eq(line, "show_temp")) {
 		uint8_t temp, hum;
 		uint8_t err = dht_read(&hum, &temp);
@@ -132,6 +173,30 @@ void handle_cmd(char* line) {
 		} else {
 			uart_printf("DHT11 error code: %u\r\n", (unsigned int) err);
 		}
+	} else if(str_eq(line, "sys_uptime")) {
+			uart_puts("\033[2J\033[H");
+			uart_puts("System Up time (Press x to return)\r\n");
+			while(1) {
+				if(uart_available()) {
+					char c = uart_getc_buffered();
+					if(c == 'x') {
+						uart_puts("\033[2J\033[H");
+						break;
+					}
+				}
+
+				if (new_second) {
+					new_second = 0;
+
+					uart_puts("\033[1K\r");
+					uart_printf("%u days %u hours %u minutes %u seconds              ",
+							(unsigned int) day_counter, 
+							(unsigned int) ((second_counter / 3600) % 24),
+							(unsigned int) ((second_counter / 60) % 60),
+							(unsigned int) (second_counter % 60)
+						);
+				}
+			}
 	} else {
 			uart_printf("Unknown command. Type 'help'\r\n");
 	}
