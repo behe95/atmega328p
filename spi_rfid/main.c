3a1b6263bbf775ddac11294814e1230c2f015a9c
behe95 <65931945+behe95@users.noreply.github.com>
2026-08-19 01:19:56 -0400

    REFACTOR: uart, spi setup-TEST: rc522 chip registers

--- spi_rfid/main.c
+++ spi_rfid/main.c
@@ -1,7 +1,7 @@
 /**
 * RFID RC522
 *	Syed M.
-*	Datasheet: https://www.alldatasheet.com/view.jsp?Searchword=RC522
+*	Datasheet: https://www.nxp.com/docs/en/data-sheet/MFRC522.pdf
 *		Sections:
 *			10.2 SPI Compatible Interface
 *			9.1	MFRC522 Registers Overview
@@ -11,27 +11,106 @@
 #include "uart.h"
 #include <avr/io.h>
 #include <avr/interrupt.h>
+#include <util/delay.h>
+
 
 #define F_CPU 16000000L
-#define _RFID_ADD_WRITE 0x00
+#define _RFID_ADDR_WRITE 0x00
 #define _RFID_ADDR_READ 0x80	 // 10000000
 #define _RFID_ADDR_MASK 0x7E		// 01111110
 
 #define _RFID_VersionReg 0x37
 #define _SPI_DUMMY_DATA 0x00
 
+#define _RFID_CommandReg 0x01
+#define _RFID_FIFODataReg 0x09	//FIFO buffer
+#define _RFID_FIFOLevelReg 0x0A	// FIFO length tracer
+
+#define _RFID_ErrorReg 0x06
+#define _RFID_BufferOvfl 4
+
+#define _RFID_WaterLevelReg 0x0B
+#define _RFID_WaterLevel_MASK 0x3F//00111111
+
+#define _RFID_Status1Reg 0x07
+#define _RFID_HiAlert 0x01
+#define _RFID_LoAlert 0x00
+
+
+void setup_uart();
+void setup_spi();
 uint8_t spi_transfer(uint8_t data);
 uint8_t rc522_read_reg(uint8_t reg);
-uint8_t rc522_write_reg(uint8_t reg, uint8_t val);
+void rc522_write_reg(uint8_t reg, uint8_t val);
 
 
 int main(void) {
 		
+		setup_uart();
+		setup_spi();
+
+		_delay_us(40); // 16.11
+		rc522_write_reg(_RFID_CommandReg, 0x0F);		// soft reset		
+		rc522_write_reg(_RFID_FIFOLevelReg, 0x80);
+	
+		rc522_write_reg(_RFID_WaterLevelReg, 0x02 & _RFID_WaterLevel_MASK);	// set water level
+
+
+
+		uint8_t data[] = {0x01, 0x02, 0x03, 0x04, 0x05};
+	
+		
+
+		// write to FIFO
+		for(uint8_t i = 0; i < 68; i++) {
+			
+
+			rc522_write_reg(_RFID_FIFODataReg, data[i%5]);
+			uint8_t level = rc522_read_reg(_RFID_FIFOLevelReg);
+			uart_printf("Wrote byte %u FIFOLevel = %u\r\n", i, level);
+			if(rc522_read_reg(_RFID_ErrorReg) & (1 << _RFID_BufferOvfl)) {
+				uart_printf("Error happened!!!!\r\n");
+			}
+			
+			
+			if(rc522_read_reg(_RFID_Status1Reg) & (1 << _RFID_LoAlert)) {
+				uart_printf("FIFO current bytes is less than %u\r\n"
+						, rc522_read_reg(_RFID_WaterLevelReg) & _RFID_WaterLevel_MASK);
+			} else if (rc522_read_reg(_RFID_Status1Reg) & (1 << _RFID_HiAlert)) {
+				uart_printf("FIFO remaining bytes is less than %u\r\n",
+						rc522_read_reg(_RFID_WaterLevelReg) & _RFID_WaterLevel_MASK);
+			}
+
+
+		}
+	
+/*	
+		//rc522_write_reg(_RFID_FIFOLevelReg, 0x80);
+		// read from FIFO
+		uint8_t level = rc522_read_reg(_RFID_FIFOLevelReg);
+		for(uint8_t i = 0; i < level; i++) {
+			uint8_t byte = rc522_read_reg(_RFID_FIFODataReg);
+			uint8_t currlvl = rc522_read_reg(_RFID_FIFOLevelReg);
+			uart_printf("Byte = %u Level = %u\r\n", byte, currlvl);
+		}
+*/
+		while(1) {
+
+		}
+}
+
+void setup_uart() {
+
+
 		// uart config
 		UBRR0H = 0;
 		UBRR0L = 103;
 		UCSR0B = (1 << TXEN0) | (1 << RXEN0) | (1 << RXCIE0);
 		UCSR0C = (1 << UCSZ01) | (1 << UCSZ00);
+
+}
+
+void setup_spi() {
 
 
 		// SPI config
@@ -42,29 +121,7 @@ int main(void) {
 		
 		DDRD |= (1 << DDD3);
 		PORTD |= (1 << PORTD3);
-
-		int counter = 0;
-
-		while(1) {
-			//uart_puts("Hello");
-
-		counter++;
-
-		//uint8_t rfid_version = rc522_read_reg(_RFID_VersionReg);
-
-		//uart_printf("Version: %u\r\n", rfid_version);
-		
-		//	uint8_t value = rc522_read_reg(0x01);
-		//	uart_printf("CommandReg: %u\r\n", value);
-		//	if(counter >= 10) break;
-
-
-			
-
-
-		}
 }
-
 
 uint8_t spi_transfer(uint8_t data) {
 	SPDR = data;
@@ -80,9 +137,9 @@ uint8_t rc522_read_reg(uint8_t reg) {
 }
 
 
-uint8_t rc522_write_reg(uint8_t reg, uint8_t val) {
+void rc522_write_reg(uint8_t reg, uint8_t val) {
 	PORTB &= ~(1 << PORTB2);
-	spi_transfer(_RFID_ADDR_WRITE | ((reg << 1) | _RFID_ADDR_MASK));
-	spi_transfer(value);
+	spi_transfer(_RFID_ADDR_WRITE | ((reg << 1) & _RFID_ADDR_MASK));
+	spi_transfer(val);
 	PORTB |= (1 << PORTB2);
 }
